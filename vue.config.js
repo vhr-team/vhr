281d1ba3540fe2d308b993b683733dd556ce47c9
xhh <303158131@qq.com>
2020-04-08 17:06:27 +0800

    修改代理对象，target 为服务器地址 8001

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+/* 代理对象 */
+let proxyObj = {}
+
+/*拦截Http请求 */
+proxyObj['/'] = {
+    ws: false,
+    /* 目标的转发地址，就是将拦截的Http请求转发到哪里去 */
+    target: 'http://localhost:8001',
+    changeOrigin: true,
+    /* 请求地址要不要重写 */
+    pathRewrite: {
+        /* 不修改 */
+        '^/': ''
+    }
+}
+
+module.exports = {
+
+    /* 开发环境 */
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        proxy: proxyObj
+    }
+}
